8116b39f6f80193fa47808eb370295fbbd8e6c6e
tobias <tobias.omondi@student.moringaschool.com>
2025-04-14 09:54:51 +0300

    variable  operator

---
+++ Introdution/Variable.py
@@ -0,0 +1,21 @@
+# Today on 14th April 2025 we will work on varibles and conditional statements
+# Creating a variable with Assignments
+
+variable_name = "value"
+
+# The value can be "strings, numbers, list , dictionaries"
+# Variables store memory rather than objects
+
+
+# THE ASSIGNMENT OPERATOR
+# Operators are special symbols that perform mathematical, logical and bitwise operations in a programming language
+
+number = 40
+id(number) # used to inspect the memory address stored in a given varible
+
+greetings = "Hello, Toby!"
+total = 10 + 7
+is_true = 43 < 100
+
+print(number)
+
